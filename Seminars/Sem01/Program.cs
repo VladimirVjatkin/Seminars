650d66daacddd9b56aaf60587c6a7d1cbc4841b9
Vladimir Vjatkin <viatkin@volny.cz>
2024-04-05 11:23:18 +0200

    we make seminars folder

---
+++ Seminars/Sem01/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+// Console.WriteLine("Tipe number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Quart {a} = {a*a}");
+
+// using System.Timers;
+
+// Console.WriteLine("Tipe first number = ");
+// int d1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Tipe second number = ");
+// int d2 = Convert.ToInt32(Console.ReadLine());
+
+// if (d2 == d1*d1) 
+// {Console.WriteLine($"YES num1 {d1}");}
+// else {Console.WriteLine("NO");}
+
+// Console.Write("Tipe N = ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int start = -n;
+// while (start<=n)
+// {
+// Console.Write(start +"");
+// start++;
+// }
+
+Console.Write("Tipe N = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num>99 && num<1000)
+{
+int ed = num % 10;
+int sot = num / 100;
+
+Console.WriteLine($"sum of {ed} and {sot} -> {ed+sot}");
+}
+else { Console.WriteLine("not tri znaka");}
