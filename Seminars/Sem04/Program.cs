901b6cf9815c95750e7ec6aff9fd8eb2e5544a4c
Vladimir Vjatkin <viatkin@volny.cz>
2024-04-18 20:45:18 +0200

    add sem04 and sem05

---
+++ Seminars/Sem04/Program.cs
@@ -0,0 +1,264 @@
+﻿ 
+/*
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);//[min, max] [0, 9]
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+bool isPrime(int num)
+{
+    if(num==1) return false;
+    for (int i = 2; i <= num/2; i++)
+    {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int CountPrime(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(isPrime(array[i])) count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value of arr element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value of arr element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(size, min, max);
+PrintArray(array)
+System.Console.WriteLine(CountPrime(array));
+*/
+
+/*
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);//[min, max] [0, 9]
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+bool IsPrime(int num)
+{
+    if(num == 1) return false; 
+    for (int i = 2; i <= num/2; i++)
+    {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int CountPrime(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(IsPrime(array[i])) count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value of arr element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value of arr element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(size, min, max);
+PrintArray(array);
+System.Console.WriteLine(CountPrime(array));
+*/
+
+// 002
+// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+// Пример
+// [1 5 11 21 81 4 0 91 2 3]
+// => 2
+/*
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);//[min, max] [0, 9]
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+bool NumCounter(int num)
+{
+    if(num == 1) return false; 
+    for (int i = 2; i <= num/2; i++)
+    {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int CountPrime(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(IsPrime(array[i])) count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value of arr element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value of arr element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(size, min, max);
+PrintArray(array);
+System.Console.WriteLine(CountPrime(array));
+
+*/
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);//[min, max] [0, 9]
+    }
+    return array;
+}
+
+int[] InputArray()
+{
+    System.Console.WriteLine("Input array size: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write("Input " + (i + 1) +" element: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+bool IsPrime(int num)
+{
+    if(num == 1) return false; 
+    for (int i = 2; i <= num/2; i++)
+    {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int CountPrime(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(IsPrime(array[i])) count++;
+    }
+    return count;
+}
+
+int NumCounter(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 10 == 1 && array[i] % 7 == 0) count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value of arr element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value of arr element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = InputArray();
+// PrintArray(array);
+// System.Console.WriteLine(NumCounter(array));
+
+/*
+Заполните массив на N (вводится с консоли, не более 8)
+случайных целых чисел от 0 до 9.
+Сформируйте целое число, которое будет состоять из цифр из
+массива. Старший разряд числа находится на 0-м индексе,
+младший – на последнем.
+Пример
+[1 3 2 4 2 3] => 132423
+[2 3 1] => 231
+*/
+
+// int ConvertToNum(int[] array)
+// {
+//     int result = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         result = result + array[i] * (int)Math.Pow(10, array.Length - 1 - i);
+//     }
+//     return result;
+// }
+
+// Список чатов телеграм есть справа на странице семинара. Вот ссылки телеграм-каналов:
+// Группа 6132: https://t.me/+nlAF2HSmPsNjYzIy
+// Группа 6339: https://t.me/+o8rGKzKB9R85NzBi
+// Группа 6342: https://t.me/+mdgpMzwa-_0zY2Yy
---
+++ Seminars/Sem05/Program.cs
@@ -0,0 +1,178 @@
+﻿//Задайте двумерный массив. Найдите элементы, у которых оба
+//индекса чётные, и замените эти элементы на их квадраты.
+/*
+int[,] CreateMatrix(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];
+    for (int for (int i = 0; i < rows; i++)
+    {
+        
+    for (int j = 0; j < colums; j++)
+    {
+        matrix[i,j] = new Random().Next(10,100);
+    }
+return matrix;  
+    }      
+
+
+}*/
+/////////////////////
+///
+// int[,] CreateMatrix(int rows, int columns, int min, int max)
+// {
+//     int[,] matrix = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             matrix[i, j] = new Random().Next(min, max + 1);//[min, max] [0, 9]
+//         }
+//     }
+//     return matrix;
+// }
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// int[,] EvenIndxSquare(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if(i % 2 == 0 && j % 2 == 0)
+//             {
+//                 matrix[i, j] = matrix[i, j] * matrix[i, j];
+//             }
+//         }
+//     }
+//     return matrix;
+// }
+
+// // System.Console.WriteLine("Input number of rows: ");
+// // int rows = Convert.ToInt32(Console.ReadLine());
+// // System.Console.WriteLine("Input number of columns: ");
+// // int columns = Convert.ToInt32(Console.ReadLine());
+// // System.Console.WriteLine("Input minimal value of arr element: ");
+// // int min = Convert.ToInt32(Console.ReadLine());
+// // System.Console.WriteLine("Input maximal value of arr element: ");
+// // int max = Convert.ToInt32(Console.ReadLine());
+
+// // int[,] matrix = CreateMatrix(rows, columns, min, max);
+// // PrintMatrix(matrix);
+// // PrintMatrix(EvenIndxSquare(matrix));
+
+// // Задайте двумерный массив. Найдите сумму элементов,
+// // находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// // т.д.
+// // Пример
+// // 2 3 4 3
+// //  4 3 4 1 => 2 + 3 + 5 = 10
+// //  2 9 5 4
+
+// int SumMatrix(int[,] matrix)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if(i == j)
+//             {
+//                 sum += matrix[i, i];
+//             }
+//         }
+//     }
+//     return sum;
+// }
+
+
+// System.Console.WriteLine("Input number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input minimal value of arr element: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input maximal value of arr element: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] matrix = CreateMatrix(rows, columns, min, max);
+// PrintMatrix(matrix);
+// System.Console.WriteLine(SumMatrix(matrix));
+
+/*****************************/
+
+// Задайте двумерный массив из целых чисел. Сформируйте новый
+// одномерный массив, состоящий из средних арифметических
+// значений по строкам двумерного массива.
+// Пример
+// 2 3 4 3
+// 4 3 4 1
+// 2 3 4 3
+// 4 3 4 1 => [3 3 5]
+// 2 9 5 4
+
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);//[min, max] [0, 9]
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+
+
+double[] MidlSum(int[,] matrix)
+{
+
+    double[] arrow = new double[matrix.GetLength(0)];
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+
+        }
+        arrow[i] = (double) sum / matrix.GetLength(1);
+        sum = 0;
+    }
+    return arrow;
+}
+System.Console.WriteLine("Input number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value of arr element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value of arr element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix01 = CreateMatrix(rows, columns, min, max);
+PrintMatrix(matrix01);
+
+PrintArray(MidlSum(matrix01));
